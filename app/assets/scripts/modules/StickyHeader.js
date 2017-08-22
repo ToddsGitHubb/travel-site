7a9f715d4c05f5880e0ea1f669e5c893b1e8cddf
Todd <KnightT@sinsdev.sins.caldoj.gov>
2017-08-22 15:56:39 -0700

    Added browser support

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 import smoothScroll from 'jquery-smooth-scroll';
-
+ 
 class StickyHeader {
   constructor() {
     this.lazyImages = $(".lazyload");
