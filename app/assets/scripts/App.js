32cf26c654c0506cbc4d3b980308b5ddb1e5e368
Todd <KnightT@sinsdev.sins.caldoj.gov>
2017-08-22 09:30:39 -0700

    Completed reveal on scroll.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,25 +1,7 @@
-/*
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-// es6 import
-import Person from './modules/Person';
-
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " you owe uncle Sam - $.01");
-  }
-}
-
-var dude = new Person("Dumble Head", "brown");
-dude.greet();
-
-var jane = new Adult("Bubble Head", "yellow");
-jane.greet();
-jane.payTaxes();
-
-new Person("Hubble Head", "Blue").greet();
-*/
-
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
